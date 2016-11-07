cfbf146c4db2e73f435a64b34a355b8750cb2e68
sunyang <sunyang@91taogu.com>
2016-11-07 15:41:31 +0800

    文件提交

---
+++ 模型设计与KVC/模型设计与KVC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  模型设计与KVC
+//
+//  Created by SunYang on 16/11/7.
+//  Copyright © 2016年 SunYang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
